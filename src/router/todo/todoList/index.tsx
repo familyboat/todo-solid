86b51a1f0813372ca7a3c2376ce27c4db94fb205
familyboat <2015301110129@whu.edu.cn>
2025-10-20 19:24:26 +0800

    use auto animate for list animation

--- src/router/todo/todoList/index.tsx
+++ src/router/todo/todoList/index.tsx
@@ -1,37 +1,42 @@
-import { TransitionGroup } from 'solid-transition-group'
 import styles from './index.module.css'
-import './index.css'
 import { For, onMount } from 'solid-js'
 import { isCreated, isDeleted, isDone, loadFromDb, todosStore } from '@/stores/todo'
 import TodoItem from '../todoItem'
+import { createAutoAnimate } from '@formkit/auto-animate/solid'
 
 export default function TodoList() {
   onMount(() => {
     loadFromDb()
   })
 
+  const createdTodos = () => {
+    return todosStore.todos.filter((todo) => isCreated(todo))
+  }
+
+  const doneTodos = () => {
+    return todosStore.todos.filter((todo) => isDone(todo))
+  }
+
+  const deletedTodos = () => {
+    return todosStore.todos.filter((todo) => isDeleted(todo))
+  }
+
+  const [parent] = createAutoAnimate()
+
   return (
     <>
-      <ul class={styles.list}>
-        <TransitionGroup name="fade-todo">
-          <header>已创建：</header>
-          <For each={todosStore.todos.filter((todo) => isCreated(todo))}>
-            {(todo, index) => <TodoItem index={index()} todo={todo} />}
-          </For>
-          <hr />
+      <ul class={styles.list} ref={parent}>
+        <header>已创建：</header>
+        <For each={createdTodos()}>{(todo, index) => <TodoItem index={index()} todo={todo} />}</For>
+        <hr />
 
-          <header>已完成：</header>
-          <For each={todosStore.todos.filter((todo) => isDone(todo))}>
-            {(todo, index) => <TodoItem index={index()} todo={todo} />}
-          </For>
-          <hr />
+        <header>已完成：</header>
+        <For each={doneTodos()}>{(todo, index) => <TodoItem index={index()} todo={todo} />}</For>
+        <hr />
 
-          <header>已删除：</header>
-          <For each={todosStore.todos.filter((todo) => isDeleted(todo))}>
-            {(todo, index) => <TodoItem index={index()} todo={todo} />}
-          </For>
-          <hr />
-        </TransitionGroup>{' '}
+        <header>已删除：</header>
+        <For each={deletedTodos()}>{(todo, index) => <TodoItem index={index()} todo={todo} />}</For>
+        <hr />
       </ul>
     </>
   )
